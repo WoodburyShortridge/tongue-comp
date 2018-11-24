a1362cc418833ea694993a03af16fd096e3fb68e
woodbury shortridge <whshortridge@gmail.com>
2018-11-24 13:22:14 -0500

    proof

--- main.js
+++ main.js
@@ -9,3 +9,132 @@ const NUM_CLASSES = 3;
 const IMAGE_SIZE = 227;
 // K value for KNN
 const TOPK = 10;
+
+class Main {
+  constructor() {
+    // Initiate variables
+    this.infoTexts = [];
+    this.videoPlaying = false;
+
+    // Initiate deeplearn.js math and knn classifier objects
+    this.bindPage();
+
+    // Create video element that will contain the webcam image
+    this.video = document.createElement('video');
+    this.video.setAttribute('autoplay', '');
+    this.video.setAttribute('playsinline', '');
+
+    // Add video element to DOM
+    document.body.appendChild(this.video);
+
+    // Create training buttons and info texts
+    for (let i = 0; i < NUM_CLASSES; i++) {
+      const div = document.createElement('div');
+      document.body.appendChild(div);
+      div.style.marginBottom = '10px';
+
+      // Create info text
+      const infoText = document.createElement('span')
+      infoText.innerText = " No examples added";
+      div.appendChild(infoText);
+      this.infoTexts.push(infoText);
+    }
+
+    // Setup webcam
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        this.video.srcObject = stream;
+        this.video.width = IMAGE_SIZE;
+        this.video.height = IMAGE_SIZE;
+
+        this.video.addEventListener('playing', () => this.videoPlaying = true);
+        this.video.addEventListener('paused', () => this.videoPlaying = false);
+      })
+  }
+
+  async bindPage() {
+    this.knn = knnClassifier.create();
+    this.mobilenet = await mobilenetModule.load();
+    // Get image data from video element
+    for (let i = 0; i < NUM_CLASSES; i++) {
+      let image = new Image();
+      if (i === 0) {
+        image.src = 'images/LEFT.png';
+      }
+      else if (i === 1) {
+        image.src = 'images/RIGHT.png';
+      }
+      else {
+        image.src = 'images/NONE.png';
+      }
+      image.width = IMAGE_SIZE;
+      image.height = IMAGE_SIZE;
+
+      image.addEventListener('load', () => {
+        document.body.appendChild(image);
+        let imageTf = tf.fromPixels(image);
+        let infer = () => this.mobilenet.infer(imageTf, 'conv_preds');
+        // Train class if one of the buttons is held down
+        let logits = infer();
+        // Add current image to classifier
+        this.knn.addExample(logits, i)
+
+        // Dispose image when done
+        imageTf.dispose();
+        if (logits != null) {
+          logits.dispose();
+        }
+      }, false);
+    };
+    this.start();
+  }
+
+  start() {
+    if (this.timer) {
+      this.stop();
+    }
+    this.video.play();
+    this.timer = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  stop() {
+    this.video.pause();
+    cancelAnimationFrame(this.timer);
+  }
+
+  async animate() {
+    if (this.videoPlaying) {
+
+      const numClasses = this.knn.getNumClasses();
+      if (numClasses > 0) {
+
+        // If classes have been added run predict
+        const image = tf.fromPixels(this.video);
+        let infer = () => this.mobilenet.infer(image, 'conv_preds');
+        let logits = infer();
+        const res = await this.knn.predictClass(logits, TOPK);
+
+        for (let i = 0; i < NUM_CLASSES; i++) {
+
+          // The number of examples for each class
+          const exampleCount = this.knn.getClassExampleCount();
+
+          // Make the predicted class bold
+          if (res.classIndex == i) {
+            this.infoTexts[i].style.fontWeight = 'bold';
+          } else {
+            this.infoTexts[i].style.fontWeight = 'normal';
+          }
+
+          // Update info text
+          if (exampleCount[i] > 0) {
+            this.infoTexts[i].innerText = ` ${exampleCount[i]} examples - ${res.confidences[i] * 100}%`
+          }
+        }
+      }
+    }
+    this.timer = requestAnimationFrame(this.animate.bind(this));
+  }
+}
+
+window.addEventListener('load', () => new Main());
