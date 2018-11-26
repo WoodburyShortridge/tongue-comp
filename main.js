1a5f058f38482052d7662d8f08a583a90571c062
woodbury shortridge <whshortridge@gmail.com>
2018-11-26 12:11:04 -0500

    webpack config

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import "@babel/polyfill";
 import * as mobilenetModule from '@tensorflow-models/mobilenet';
 import * as tf from '@tensorflow/tfjs';
 import * as knnClassifier from '@tensorflow-models/knn-classifier';
 import * as data from './training/data.json';
+import style from './main.css';
 
 // Number of classes to classify
 const NUM_CLASSES = 3;
-// Webcam Image size. Must be 227.
+// Webcam and Image size. Must be 227.
 const IMAGE_SIZE = 227;
 // K value for KNN
 const TOPK = 10;
@@ -35,7 +35,7 @@ class Main {
       div.style.marginBottom = '10px';
 
       const infoText = document.createElement('span')
-      infoText.innerText = "Data loading...";
+      infoText.innerText = "Data loading....";
       div.appendChild(infoText);
       this.infoTexts.push(infoText);
     }
